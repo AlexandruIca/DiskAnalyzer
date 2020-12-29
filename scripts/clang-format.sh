ed6017c1aed9be3cbdb7fdaace98d129055c858f
Alexandru Ica <alexandruica703@gmail.com>
2020-12-29 20:03:34 +0200

    Run static analysis for the whole project

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-./scripts/run-clang-format.py --clang-format-executable=${CLANG_FORMAT_PATH} -r server/ client/
+./scripts/run-clang-format.py --clang-format-executable=${CLANG_FORMAT_PATH} -r server/ client/ shared/ tests/
